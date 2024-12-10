9d637f116c6078b188391aa11539d59dd5bc0955
VasilNadiradze <vasil.nadiradze@gmail.com>
2024-12-10 22:25:57 +0400

    ხარჯების ცხრილი

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import Form from "./components/Form";
+import { useState } from "react";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "კლავიატურა", amount: 10, category: "ტექნიკა" },
+    { id: 2, description: "პროცესორი", amount: 100, category: "ტექნიკა" },
+    { id: 3, description: "პური", amount: 1, category: "საკვები" },
+    { id: 4, description: "ლიმონათი", amount: 10, category: "სასმელი" },
+  ]);
+
   return (
     <div>
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => {
+          setExpenses(expenses.filter((e) => e.id !== id));
+        }}
+      />
     </div>
   );
 }
